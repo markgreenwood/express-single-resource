857cfa0d3c6f160c55f29405305c23d3521b7288
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-11-01 17:45:17 -0700

    Skeleton for error handler unit tests

---
+++ test/error-handler.test.js
@@ -0,0 +1,9 @@
+const errorHandler = require('../lib/error-handler');
+
+describe ('errorHandler unit tests', () => {
+
+  it ('reports errors when given a code and a message', () => {
+    
+  });
+
+});
